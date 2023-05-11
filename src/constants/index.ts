e27c0afb296f66f9fd501874e6bad75297a8be04
Chris Huber <chris.huber386@gmail.com>
2023-05-10 21:55:06 -0400

    populate src folder

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const MY_SLEEPER_USER_ID = '59459773886842880';
+export const MY_SLEEPER_USERNAME = 'chubes2';
---
+++ src/types/dynastic/model/dynastic.ts
@@ -0,0 +1,12 @@
+export type Player = {
+    id: string
+    firstName: string
+    lastName: string
+    headshot: string
+    age:number
+    isRookie: boolean
+    isTradeable: boolean
+    isPrime: boolean
+    position: string //enum
+    rank: number
+}
