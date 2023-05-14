9db2d55078e55bafb7037536a6bdc795a7588a85
Chris Huber <chris.huber386@gmail.com>
2023-05-14 01:09:45 -0400

    webpack dev works with js files

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
   mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+
+  plugins: [new HtmlWebpackPlugin({title: 'Development'})],
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
   module: {
     rules: [
       {
@@ -29,7 +29,6 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   optimization: {
     runtimeChunk: 'single',
