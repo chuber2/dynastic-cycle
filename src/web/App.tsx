2d9e6423dbcb18992454e93dfa0183b95d23d141
chuber2 <chris.huber386@gmail.com>
2023-09-22 21:51:35 -0400

    install base ui

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import MainPage from './components/MainPage/MainPage';
 
-interface TitleProps {
-  title: string; // required
-  subtitle?: string; // optional
-}
-
 interface AppProps {}
 
 // const Title: React.FC<TitleProps> = ({title, subtitle}) => {
