25dee9e31376f2e274135cc71eaea71b7a6ddf43
Chris Huber <chris.huber386@gmail.com>
2026-05-19 01:59:20 -0400

    improve scripting exploration with abstracted function

--- server/scripts/explore-sleeper.ts
+++ server/scripts/explore-sleeper.ts
@@ -1,6 +1,19 @@
-const response = await fetch("https://api.sleeper.app/v1/user/chubes2");
-const data = await response.json();
-console.log(data);
+const fetcher = async (url: string | URL): Promise<unknown> => {
+  const response = await fetch(url);
+  if(!response.ok) {
+    throw new Error(`HTTP ${response.status} for ${url}`);
+  }
+  const data = await response.json();
+  return data;
+}
+
+const user_id = process.env.SLEEPER_USER_ID
+if(!user_id) {
+  throw new Error('Sleeper USER_ID not set in .env')
+}
+
+const userRes = await fetcher(`https://api.sleeper.app/v1/user/${user_id}`);
+console.log(userRes)
 
 export { };
 
