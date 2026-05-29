4426a8702a1f2d657e2141a70658785673a7d591
Chris Huber <chris.huber386@gmail.com>
2026-05-29 18:04:45 -0400

    update config files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,7 @@
+import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'vite';
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -6,5 +9,11 @@ export default defineConfig({
       '/api': 'http://localhost:3000',
     },
     port: 8080,
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
   },
 });
