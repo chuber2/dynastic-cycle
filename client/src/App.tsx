7d51178225d5bc7b88d5871619e5fd43323dc128
Chris Huber <chris.huber386@gmail.com>
2026-05-29 18:09:43 -0400

    import some buttons!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import {Button} from '@/components/ui/button';
+
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className="p-8">
+      <Button>Click Me!</Button>
+      <Button variant="outline" className="ml-2">
+        Outline
+      </Button>
+      <Button variant="destructive" className="ml-2">
+        Destructive
+      </Button>
+    </div>
+  );
 };
 
 export default App;
