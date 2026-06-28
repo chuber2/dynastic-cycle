10b1a5628cd6c068139bf1171c0b065e55622353
Chris Huber <chris.huber386@gmail.com>
2026-06-28 14:33:14 -0400

    move position classes to the right file.

--- client/src/lib/teamClasses.ts
+++ client/src/lib/teamClasses.ts
@@ -33,30 +33,3 @@ export const TEAM_BG: Record<string, string> = {
   TEN: 'bg-team-ten',
   WAS: 'bg-team-was',
 };
-
-export const POSITION_BG: Record<string, string> = {
-  QB: 'bg-pos-qb',
-  RB: 'bg-pos-rb',
-  WR: 'bg-pos-wr',
-  TE: 'bg-pos-te',
-  K: 'bg-pos-k',
-  DEF: 'bg-pos-def',
-};
-
-export const POSITION_TEXT: Record<string, string> = {
-  QB: 'text-pos-qb',
-  RB: 'text-pos-rb',
-  WR: 'text-pos-wr',
-  TE: 'text-pos-te',
-  K: 'text-pos-k',
-  DEF: 'text-pos-def',
-};
-
-export const POSITION_BORDER: Record<string, string> = {
-  QB: 'border-pos-qb',
-  RB: 'border-pos-rb',
-  WR: 'border-pos-wr',
-  TE: 'border-pos-te',
-  K: 'border-pos-k',
-  DEF: 'border-pos-def',
-};
